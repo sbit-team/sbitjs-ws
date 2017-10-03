f942755ec85aa33d97db00baaf65868b59071592
SBit developer <sbit.devteam@gmail.com>
2017-10-03 18:29:36 +0300

    some logs wrapped by DEBUG

--- cjs/src/SBitApi.js
+++ cjs/src/SBitApi.js
@@ -4,6 +4,8 @@ exports.__esModule = true;
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
+if (global.DEBUG === undefined) global.DEBUG = false;
+
 var SBitApi = function () {
     function SBitApi(ws_rpc, api_name) {
         _classCallCheck(this, SBitApi);
@@ -17,7 +19,7 @@ var SBitApi = function () {
 
         var self = this;
         return this.ws_rpc.call([1, this.api_name, []]).then(function (response) {
-            console.log("[SBitApi.js:11] ----- SBitApi.init ----->", _this.api_name, response);
+            if (DEBUG) console.log("[SBitApi.js:11] ----- SBitApi.init ----->", _this.api_name, response);
             self.api_id = response;
             return self;
         });
@@ -25,7 +27,7 @@ var SBitApi = function () {
 
     SBitApi.prototype.exec = function exec(method, params) {
         return this.ws_rpc.call([this.api_id, method, params]).catch(function (error) {
-            console.log("!!! SBitApi error: ", method, params, error, JSON.stringify(error));
+            if (DEBUG) console.log("!!! SBitApi error: ", method, params, error, JSON.stringify(error));
             throw error;
         });
     };
